9d9b17ad1d40d7bc1b2809518198786293b3a11c
Veffica <jessyca.1991@gmail.com>
2022-12-30 21:59:58 -0300

    Adding first files

---
+++ Game.js
@@ -0,0 +1,183 @@
+import * as THREE from '../libs/three137/three.module.js';
+import { GLTFLoader } from '../libs/three137/GLTFLoader.js';
+//import { RGBELoader } from '../libs/three137/RGBELoader.js';
+import { NPCHandler } from './NPCHandler.js';
+//import { LoadingBar } from '../libs/LoadingBar.js';
+import { Pathfinding } from '../libs/pathfinding/Pathfinding.js';
+import {OrbitControls} from '../libs/three137/OrbitControls.js';
+
+class Game{
+	constructor(){
+		const container = document.createElement( 'div' );
+		document.body.appendChild( container );
+        
+		this.clock = new THREE.Clock();
+
+        //this.loadingBar = new LoadingBar();
+        //this.loadingBar.visible = false;
+
+		this.assetsPath = '../../assets/';
+        
+		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.001, 5000 );
+
+		this.camera.position.set( -1.3, 1.5, -10 );
+		
+		let col = 0x201510;
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color( col );
+		this.scene.fog = new THREE.Fog( col, 100, 200 );
+
+		const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+		this.scene.add(ambient);
+
+        const light = new THREE.DirectionalLight();
+        light.position.set( 4, 20, 20 );
+		light.target.position.set(-2, 0, 0);
+		light.castShadow = true;
+		//Set up shadow properties for the light
+		light.shadow.mapSize.width = 1024; 
+		light.shadow.mapSize.height = 512; 
+		light.shadow.camera.near = 0.5; 
+		light.shadow.camera.far = 50;
+		const d = 30; 
+		light.shadow.camera.left = -d;
+		light.shadow.camera.bottom = -d*0.25;
+		light.shadow.camera.right = light.shadow.camera.top = d;
+		this.scene.add(light);
+		this.light = light;
+	
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+		container.appendChild( this.renderer.domElement );
+        //this.setEnvironment();
+
+		const controls = new OrbitControls(this.camera, this.renderer.domElement)
+		controls.update();
+		
+		this.load();
+		
+		window.addEventListener( 'resize', this.resize.bind(this) );
+	}
+
+	initPathfinding(navmesh){
+		//let dinner = new THREE.Vector3(1.63,-0.13,-6.60);
+		//let kitchen = new THREE.Vector3(-4.53,-0.13,-5.19);
+		//let tvRoom = new THREE.Vector3(-2.67,-0.13,-8.87);
+		//let corridor = new THREE.Vector3(-1.21,-0.16,-1.15);
+
+		this.waypoints = [
+			new THREE.Vector3(1.63,-0.13,-6.60), //cozinha
+			new THREE.Vector3(-4.53,-0.13,-5.19), //porta de entrada
+			new THREE.Vector3(-2.67,-0.13,-8.87), //sala
+			new THREE.Vector3(-1.21,-0.13,-1.15), //corredor
+			new THREE.Vector3(-1.1,-0.13,-5.6) //corredor perto do sofa
+		];
+		this.pathfinder = new Pathfinding();
+        this.pathfinder.setZoneData('factory', Pathfinding.createZone(navmesh.geometry, 0.02));
+		if (this.npcHandler.gltf !== undefined) this.npcHandler.initNPCs();
+	}
+	
+    resize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+    	this.camera.updateProjectionMatrix();
+    	this.renderer.setSize( window.innerWidth, window.innerHeight ); 
+    }
+    
+    /*setEnvironment(){
+        const loader = new RGBELoader().setPath(this.assetsPath);
+        const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
+        pmremGenerator.compileEquirectangularShader();
+        
+        loader.load( 'hdr/factory.hdr', 
+		texture => {
+          const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
+          pmremGenerator.dispose();
+
+          this.scene.environment = envMap;
+
+		  this.loadingBar.visible = !this.loadingBar.loaded;
+        }, 
+		xhr => {
+			this.loadingBar.update( 'envmap', xhr.loaded, xhr.total );
+		},
+		err => {
+            console.error( err.message );
+        } );
+    }*/
+    
+	load(){
+        this.loadEnvironment();
+		this.npcHandler = new NPCHandler(this);
+    }
+
+    loadEnvironment(){
+    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}`);
+        
+        //this.loadingBar.visible = true;
+		
+		// Load a glTF resource
+		loader.load(
+			// resource URL
+			'house5.glb',
+			// called when the resource is loaded
+			gltf => {
+
+				this.scene.add( gltf.scene );
+                this.factory = gltf.scene;
+
+				gltf.scene.traverse( child => {
+					if (child.isMesh){
+						if (child.name == 'navmesh'){
+							this.navmesh = child;
+							this.navmesh.geometry.rotateX( Math.PI/2 );
+							this.navmesh.quaternion.identity();
+							this.navmesh.position.set(0,0,0);
+							child.material.visible = true;
+							child.material.transparent = true;
+							child.material.opacity = 0.5;
+	
+						}
+					}
+				});
+
+				this.scene.add(this.navmesh);
+
+                this.renderer.setAnimationLoop( this.render.bind(this) );
+
+				this.initPathfinding(this.navmesh);
+
+				//this.loadingBar.visible = !this.loadingBar.loaded;
+			},
+			// called while loading is progressing
+			xhr => {
+
+				//this.loadingBar.update('environment', xhr.loaded, xhr.total);
+				
+			},
+			// called when loading has errors
+			err => {
+
+				//console.error( err );
+
+			}
+		);
+	}			
+    
+	startRendering(){
+		this.renderer.setAnimationLoop( this.render.bind(this) );
+	}
+
+	render() {
+		const dt = this.clock.getDelta();
+
+		if (this.npcHandler !== undefined ) this.npcHandler.update(dt);
+
+        this.renderer.render( this.scene, this.camera );
+
+    }
+}
+
+export { Game };
---
+++ NPCHandler.js
@@ -0,0 +1,228 @@
+import {NPC} from './NPC.js';
+import {GLTFLoader} from '../libs/three137/GLTFLoader.js';
+import {DRACOLoader} from '../libs/three137/DRACOLoader.js';
+import {Skeleton, Raycaster} from '../libs/three137/three.module.js';
+
+let counter = 0;
+let npc;
+
+class NPCHandler{
+    constructor( game ){
+        this.game = game;
+		//this.loadingBar = this.game.loadingBar;
+        this.load();
+		this.initMouseHandler();
+	}
+
+	initMouseHandler(){
+		const raycaster = new Raycaster();
+    	this.game.renderer.domElement.addEventListener( 'click', raycast, false );
+			
+    	const self = this;
+    	const mouse = { x:0, y:0 };
+
+    	
+    	function raycast(e){
+    		
+			mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+			mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+
+			//2. set the picking ray from the camera position and mouse coordinates
+			raycaster.setFromCamera( mouse, self.game.camera );    
+
+			//3. compute intersections
+			const intersects = raycaster.intersectObject( self.game.navmesh );
+			
+			if (intersects.length>0){
+				const pt = intersects[0].point;
+				console.log(pt);
+				self.npcs[0].newPath(pt, true);
+			}	
+		}
+    }
+
+    load(){
+        const loader = new GLTFLoader( ).setPath(`${this.game.assetsPath}`);
+		const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderPath( '../../libs/three137/draco/' );
+        loader.setDRACOLoader( dracoLoader );
+        //this.loadingBar.visible = true;
+		
+		// Load a GLTF resource
+		loader.load(
+			// resource URL
+			`eve2.glb`,
+			// called when the resource is loaded
+			gltf => {
+				if (this.game.pathfinder){
+					this.initNPCs(gltf);
+				}else{
+					this.gltf = gltf;
+				}
+			},
+			// called while loading is progressing
+			xhr => {
+
+				//this.loadingBar.update( 'swat-guy', xhr.loaded, xhr.total );
+
+			},
+			// called when loading has errors
+			err => {
+
+				//console.error( err );
+
+			}
+		);
+	}
+
+	initNPCs(gltf = this.gltf){
+		const gltfs = [gltf];
+		this.waypoints= this.game.waypoints; //Adcionado pra Eve ir pros waypoints
+		this.npcs = [];
+		
+		gltfs.forEach(gltf => {
+			const object = gltf.scene;
+
+			object.traverse(function(child){
+				if (child.isMesh){
+					child.castShadow = true;
+				}
+			});
+
+			const options = {
+				object: object,
+				speed: 0.8,
+				animations: gltf.animations,
+				waypoints: this.waypoints,
+				app: this.game,
+				//showPath: false,
+				zone: 'factory',
+				name: 'swat-guy',
+			};
+
+			npc = new NPC(options);
+			npc.object.position.copy(this.waypoints[3]); //Ponto inicial em que eve estará: corredor
+			npc.action = 'idle';
+			//npc.newPath(this.waypoints[4]); //Ponto que a eve vai: porta de entrada
+			//npc.object.position.set(1.63,-0.13,-6.60);
+			
+			this.npcs.push(npc);
+
+			document.addEventListener('keydown', keyDown);
+		
+			function keyDown(e){  
+				
+				if (e.code=='Space'){
+		  
+				   //console.log('pressed');
+				  const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
+				 
+				  let recognition = new SpeechRecognition(); //criou uma instance
+		  
+				  recognition.onstart = () => {  //inicia quando aperta espaço
+					  //console.log("starting listening, speak in microphone");
+				  }
+		  
+				  recognition.onspeechend = () => {  //inicia quando para de ouvir pessoa falando
+					  //console.log("stopped listening");
+					  recognition.stop();
+				  }
+		  
+				  recognition.onresult = (result) => {   //mostra no console o resultado da frase falada atraves de uma arrow function
+				  var frase = result.results[0][0].transcript;
+				  console.log(frase);
+				  
+						  switch(frase){
+									  
+							case 'go to the kitchen':
+								increment();
+								npc.newPath(npc.waypoints[0]);
+								break;
+
+							case 'door':
+								increment();  
+								npc.newPath(npc.waypoints[1]);
+								//npc.action = 'firing';
+								break;
+
+							  case 'go to the TV room':
+								//npc.action = 'Firing';
+								increment(); 
+								npc.newPath(npc.waypoints[2]);
+								break;
+							
+							case 'go back':
+								increment();
+								npc.newPath(npc.waypoints[3]);
+								break;
+
+							case 'go to the corridor':
+								increment();
+								npc.newPath(npc.waypoints[3]);
+								break;
+								
+							case 'go to the middle':
+								increment();
+								npc.newPath(npc.waypoints[4]);
+								break;
+
+							case 'die':
+								npc.action='Shot'
+								increment();
+								//npc.newPath(npc.waypoints[4]);
+								break;
+
+								
+							case 'shoot':
+								increment();
+								npc.action='Firing';
+
+								//npc.newPath(npc.waypoints[4]);
+								break;
+							
+							default:
+								npc.action = 'Idle'; 
+			  
+							  }		
+				   }			   
+				   recognition.start();
+			   }
+			}
+		});
+
+		//this.loadingBar.visible = !this.loadingBar.loaded;
+
+		this.game.startRendering();
+	}
+
+	get kitchenPoint(){
+		return this.waypoints[0];
+	}
+
+	get doorPoint(){
+		return this.waypoints[1];
+	}
+
+	get tvPoint(){
+		return this.waypoints[2];
+	}
+
+	get corridorPoint(){
+		return this.waypoints[3];
+	}
+
+	get sofaPoint(){
+		return this.waypoints[4];
+	}
+
+    update(dt){
+        if (this.npcs) this.npcs.forEach( npc => npc.update(dt) );
+    }
+}
+
+function increment(){
+	counter = counter + 1;
+	console.log(counter);
+}
+
+export { NPCHandler };
