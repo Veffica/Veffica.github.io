b15db42fd945d65569c8d5d8570953013853ad4f
Veffica <jessyca.1991@gmail.com>
2023-01-01 23:08:21 -0300

    Update NPCHandler.js

--- NPCHandler.js
+++ NPCHandler.js
@@ -169,30 +169,11 @@ class NPCHandler{
        
         if (fire) {
 			console.log('apertou o botão e deu certo')
-			keyDown();
+			voice();
 		}
     }
 
-    update(dt){
-        if (this.npcs) this.npcs.forEach( npc => npc.update(dt) );
-
-		if (this.gamepad){
-            this.gamepadHandler();
-    }
-}
-
-}
-
-function increment(){
-	counter = counter + 1;
-	console.log(counter);
-}
-
-function keyDown(e){  
-				
-	if (e.code=='Space'){
-
-	   //console.log('pressed');
+	voice(){
 	  const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
 	 
 	  let recognition = new SpeechRecognition(); //criou uma instance
@@ -269,7 +250,188 @@ function keyDown(e){
 				  }		
 	   }			   
 	   recognition.start();
-   }
+		
+	}
+
+    update(dt){
+        if (this.npcs) this.npcs.forEach( npc => npc.update(dt) );
+
+		if (this.gamepad){
+            this.gamepadHandler();
+    }
+}
+
+}
+
+function increment(){
+	counter = counter + 1;
+	console.log(counter);
+}
+
+function keyDown(e){  
+				
+	if (e.code=='Space'){
+
+	
+		voice();
+	   //console.log('pressed');
+	  /*const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
+	 
+	  let recognition = new SpeechRecognition(); //criou uma instance
+
+	  recognition.onstart = () => {  //inicia quando aperta espaço
+		  //console.log("starting listening, speak in microphone");
+	  }
+
+	  recognition.onspeechend = () => {  //inicia quando para de ouvir pessoa falando
+		  //console.log("stopped listening");
+		  recognition.stop();
+	  }
+
+	  recognition.onresult = (result) => {   //mostra no console o resultado da frase falada atraves de uma arrow function
+	  var frase = result.results[0][0].transcript;
+	  console.log(frase);
+	  
+			  switch(frase){
+						  
+				case 'go to the kitchen':
+					increment();
+					npc.newPath(npc.waypoints[0]);
+					break;
+
+				case 'go to the Kitchen':
+					increment();
+					npc.newPath(npc.waypoints[0]);
+					break;
+
+				case 'door':
+					increment();  
+					npc.newPath(npc.waypoints[1]);
+					//npc.action = 'firing';
+					break;
+
+				  case 'go to the TV room':
+					//npc.action = 'Firing';
+					increment(); 
+					npc.newPath(npc.waypoints[2]);
+					break;
+				
+				case 'go back':
+					increment();
+					npc.newPath(npc.waypoints[3]);
+					break;
+
+				case 'go to the corridor':
+					increment();
+					npc.newPath(npc.waypoints[3]);
+					break;
+					
+				case 'go to the middle':
+					increment();
+					npc.newPath(npc.waypoints[4]);
+					break;
+
+				case 'die':
+					npc.action='Shot'
+					increment();
+					//npc.newPath(npc.waypoints[4]);
+					break;
+
+					
+				case 'shoot':
+					increment();
+					npc.action='Firing';
+
+					//npc.newPath(npc.waypoints[4]);
+					break;
+				
+				default:
+					npc.action = 'Idle'; 
+  
+				  }		
+	   }			   
+	   recognition.start();
+   }*/
+}
+}	
+
+function voice(){
+	const SpeechRecognition =  window.SpeechRecognition || window.webkitSpeechRecognition;
+	 
+	  let recognition = new SpeechRecognition(); //criou uma instance
+
+	  recognition.onstart = () => {  //inicia quando aperta espaço
+		  //console.log("starting listening, speak in microphone");
+	  }
+
+	  recognition.onspeechend = () => {  //inicia quando para de ouvir pessoa falando
+		  //console.log("stopped listening");
+		  recognition.stop();
+	  }
+
+	  recognition.onresult = (result) => {   //mostra no console o resultado da frase falada atraves de uma arrow function
+	  var frase = result.results[0][0].transcript;
+	  console.log(frase);
+	  
+			  switch(frase){
+						  
+				case 'go to the kitchen':
+					increment();
+					npc.newPath(npc.waypoints[0]);
+					break;
+
+				case 'go to the Kitchen':
+					increment();
+					npc.newPath(npc.waypoints[0]);
+					break;
+
+				case 'door':
+					increment();  
+					npc.newPath(npc.waypoints[1]);
+					//npc.action = 'firing';
+					break;
+
+				  case 'go to the TV room':
+					//npc.action = 'Firing';
+					increment(); 
+					npc.newPath(npc.waypoints[2]);
+					break;
+				
+				case 'go back':
+					increment();
+					npc.newPath(npc.waypoints[3]);
+					break;
+
+				case 'go to the corridor':
+					increment();
+					npc.newPath(npc.waypoints[3]);
+					break;
+					
+				case 'go to the middle':
+					increment();
+					npc.newPath(npc.waypoints[4]);
+					break;
+
+				case 'die':
+					npc.action='Shot'
+					increment();
+					//npc.newPath(npc.waypoints[4]);
+					break;
+
+					
+				case 'shoot':
+					increment();
+					npc.action='Firing';
+
+					//npc.newPath(npc.waypoints[4]);
+					break;
+				
+				default:
+					npc.action = 'Idle'; 
+  
+				  }		
+	   }			   
+	   recognition.start();
 }
 
 export { NPCHandler };
